b11982a52c164669cc83a94bfceaf9ab70a948a4
CARD1NAL <cardinal312@github.com>
2024-03-30 22:51:01 +0600

    Initial Commit

---
+++ SwiftfulSwiftUIinPractice/SwiftfulSwiftUIinPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSwiftUIinPracticeApp.swift
+//  SwiftfulSwiftUIinPractice
+//
+//  Created by Macbook on 30/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSwiftUIinPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
